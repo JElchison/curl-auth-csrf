9d62452f6549dd1e0b8d549a6d29487635c25492
Jonathan Elchison <JElchison@Gmail.com>
2016-01-22 16:47:15 -0500

    Organize and add comments

--- curl-auth-csrf.py
+++ curl-auth-csrf.py
@@ -9,19 +9,20 @@ import requests
 import lxml.html
 
 
-VERSION = '0.1.0'
+VERSION = '1.0.0'
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(prog='curl-auth-csrf.py', description='Python tool that mimics curl, but performs a login and handles any Cross-Site Request Forgery (CSRF) tokens', formatter_class=argparse.RawDescriptionHelpFormatter)
 
-    parser.add_argument('-a', '--user-agent-str', help='User-Agent string to use', default='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36')
+    # NOTE: keep this default something common and benign
+    parser.add_argument('-a', '--user-agent-str', help='User-Agent string to use',
+                        default='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36')
 
     parser.add_argument('-i', '--login-url', required=True, help='URL that contains the login form')
-    parser.add_argument('-j', '--logout-url', help='URL to be visited to perform the logout')
 
     parser.add_argument('-f', '--form-id', help='HTML id attribute of login form')
     parser.add_argument('-p', '--password-field-name', help='name of input field containing password')
@@ -30,32 +31,27 @@ def parse_arguments():
     parser.add_argument('-u', '--success-url', help='URL substring constituting successful login')
     parser.add_argument('-t', '--success-text', help='HTML snippet constituting successful login')
 
+    parser.add_argument('-j', '--logout-url', help='URL to be visited to perform the logout')
+
     parser.add_argument('-o', '--output', metavar='FILE', type=argparse.FileType('w'), default=sys.stdout, help='write output to <file> instead of stdout')
 
-    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
     parser.add_argument('url_after_login', nargs='+')
 
-    # TODO add example
-    parser.epilog = "Actual password should be passed in via stdin.\n\nExample flow:\n\t"
+    parser.add_argument('--version', action='version', version='%(prog)s ' + VERSION)
+
+    parser.epilog = "Actual password should be passed in via stdin.\n\nSee README for examples."
 
     return parser
 
 
-def perform_login(args, password):
-    session = requests.session()
-
-    logging.info('Performing GET on login URL ...')
-    result = session.get(args.login_url, verify=False, headers={'User-Agent': args.user_agent_str})
-    logging.info("Request result = %d", result.status_code)
-    result.raise_for_status()
-
-    logging.debug('Parsing result from login request ...')
+def identify_login_form(args, result):
     tree = lxml.html.fromstring(result.text)
 
     login_form = None
     if args.form_id:
         logging.debug('Login form id field specified at command line')
         try:
+            # take the first occurrence if there are multiple with this id
             login_form = list(set(tree.xpath("//form[@id='%s']" % args.form_id)))[0]
         except IndexError:
             logging.warning("Login form id '%s' not found.  Identifying dynamically..." % args.form_id)
@@ -67,8 +63,12 @@ def perform_login(args, password):
         if len(login_forms) != 1:
             raise Exception("Cannot identify login form dynamically.  Try again with '-f'.")
         login_form = login_forms[0]
-    logging.info('Identified login form as ' + lxml.html.tostring(login_form))
 
+    logging.debug('Identified login form as ' + lxml.html.tostring(login_form))
+    return login_form
+
+
+def identify_password_field_name(args, login_form):
     if args.password_field_name:
         logging.debug('Password field specified at command line')
         password_field_name = args.password_field_name
@@ -76,9 +76,12 @@ def perform_login(args, password):
         logging.debug('No password field specified at command line.  Identifying dynamically...')
         password_field = list(set(login_form.xpath(".//input[@type='password']")))[0]
         password_field_name = password_field.name
-    logging.info('Using password_field_name = ' + password_field_name)
 
-    logging.debug('Initializing data dictionary ...')
+    logging.info('Identified password field name as ' + password_field_name)
+    return password_field_name
+
+
+def setup_data_dictionary(args, login_form, password_field_name, password):
     data = {}
     if args.data:
         logging.debug('Adding specified data to dictionary ...')
@@ -93,56 +96,44 @@ def perform_login(args, password):
     input_fields = list(set(login_form.xpath(".//input")))
     for input_field in input_fields:
         logging.debug("Parsing %s ..." % input_field)
+        # items without names can't appear in a query string
         if not input_field.name:
             logging.debug("\tNo 'name' attribute.  Continuing...")
             continue
+        # don't overwrite (things like real username and password)
         if input_field.name in data:
             logging.debug("\tField overlaps with provided data.  Continuing...")
             continue
 
-        logging.info("Adding carry-over data %s to dictionary ..." % input_field)
+        logging.debug("Adding carry-over data %s to dictionary ..." % input_field)
         if input_field.name not in data:
             data[input_field.name] = []
         data[input_field.name].append(input_field.value)
 
-    logging.debug('Dictionary = %s', data)
+    logging.debug('Data dictionary = %s', data)
+    return data
 
-    logging.debug('Parsing response URL ...')
+
+def calculate_action_url(login_form, result):
     if "://" not in login_form.action:
         url_parts = urlparse.urlparse(result.url)
-        next_url = "%s://%s%s/%s" % (url_parts.scheme, url_parts.netloc, os.path.dirname(url_parts.path), login_form.action)
+        action_url = "%s://%s%s/%s" % (url_parts.scheme, url_parts.netloc, os.path.dirname(url_parts.path), login_form.action)
     else:
-        next_url = login_form.action
-    logging.info("Calculated next_url = " + next_url)
+        action_url = login_form.action
 
-    if login_form.method == "get":
-        logging.info('Performing GET on form submission ...')
-        result = session.get(next_url, data, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
-    else:
-        logging.info('Performing POST on form submission ...')
-        result = session.post(next_url, data, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
-    logging.info("Request result = %d", result.status_code)
-    logging.info('Result URL after login = %s' % result.url)
-    result.raise_for_status()
+    logging.info("Calculated action_url as " + action_url)
+    return action_url
 
+
+def verify_login_success(args, result):
     if args.success_url and args.success_url not in result.url:
-        logging.info("content = " + result.content)
+        logging.debug("content = " + result.content)
         raise Exception("Specified success_url '%s' not in result URL '%s'.  Failed to login?" % (args.success_url, result.url))
     if args.success_text and args.success_text not in result.content:
-        logging.info("content = " + result.content)
+        logging.debug("content = " + result.content)
         raise Exception("Specified success_text not in result content.  Failed to login?")
 
-    logging.debug('Making requests of interest ...')
-    for url_after_login in args.url_after_login:
-        logging.info('Performing GET on %s ...' % url_after_login)
-        result = session.get(url_after_login, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
-        logging.info("Request result = %d", result.status_code)
-        args.output.write(result.content)
-
-    if args.logout_url:
-        logging.info('Performing GET on logout ...')
-        result = session.get(args.logout_url, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
-        logging.info("Request result = %d", result.status_code)
+    logging.info("Login was successful")
 
 
 def main():
@@ -154,7 +145,65 @@ def main():
     logging.debug('Reading password from stdin ...')
     password = sys.stdin.read()
 
-    perform_login(args, password)
+    logging.debug('Allocating session ...')
+    session = requests.session()
+
+    #################
+    # get login page
+    #################
+
+    logging.info('Performing GET on login URL ...')
+    result = session.get(args.login_url, verify=False, headers={'User-Agent': args.user_agent_str})
+    logging.info("Request result = %d", result.status_code)
+    result.raise_for_status()
+
+    ###################
+    # parse login page
+    ###################
+
+    logging.debug('Parsing result from login request ...')
+    login_form = identify_login_form(args, result)
+    password_field_name = identify_password_field_name(args, login_form)
+    logging.debug('Initializing data dictionary ...')
+    data = setup_data_dictionary(args, login_form, password_field_name, password)
+    logging.debug('Parsing response URL ...')
+    action_url = calculate_action_url(login_form, result)
+
+    ####################
+    # submit login form
+    ####################
+
+    if login_form.method == "get":
+        logging.info('Performing GET on form submission ...')
+        result = session.get(action_url, data, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
+    else:
+        logging.info('Performing POST on form submission ...')
+        result = session.post(action_url, data, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
+    logging.info("Request result = %d", result.status_code)
+    logging.info('Result URL after login = %s' % result.url)
+    result.raise_for_status()
+
+    verify_login_success(args, result)
+
+    ############################
+    # make requests of interest
+    ############################
+
+    logging.info('Making requests of interest ...')
+    for url_after_login in args.url_after_login:
+        logging.info('Performing GET on %s ...' % url_after_login)
+        result = session.get(url_after_login, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
+        logging.info("Request result = %d", result.status_code)
+        args.output.write(result.content)
+
+    #########
+    # logout
+    #########
+
+    if args.logout_url:
+        logging.info('Performing GET on logout URL ...')
+        result = session.get(args.logout_url, verify=False, headers={'Referer': result.url, 'User-Agent': args.user_agent_str})
+        logging.info("Request result = %d", result.status_code)
 
 
 if __name__ == "__main__":
